8d34725a2ea3b2c19e9752ca8a09d6e79b524d02
wanning <81507755+incalescence@users.noreply.github.com>
2024-10-08 01:28:08 +0000

    Pending changes exported from your codespace

--- routes/config.ts
+++ routes/config.ts
@@ -4,4 +4,4 @@
 export const SERVER_URL = "https://unsw-founders-tanulas.deno.dev/api";
 // This is the authentication token we'll send to the server to verify that we are authorised to
 // use the API. You'll need to run `bash token.sh` to get one of these in the workshop.
-export const SERVER_TOKEN = "";
+export const SERVER_TOKEN = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ino1MzcxMTYyQHVuc3cuZWR1LmF1IiwiZXhwIjoxNzI4MzU5NzM3fQ.UOQ1MVt0X2Q0GWuMdzW-5KE3dPo9GEWc51T7BP_9Hy4gsKjlf7YJFnCvYo8zl_1XOX3_0lnfpQm7a9ZnNi2tMA";
